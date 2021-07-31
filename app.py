2ed77d79ba0cb1bba8dd01279e61fa730356ca45
Abimbola Ojikutu <aojikutu20@aiip.alueducation.com>
2021-07-31 15:16:55 +0100

    updated app.py to retrieve data from database

--- app.py
+++ app.py
@@ -1,31 +1,26 @@
+from types import MethodDescriptorType
+from database.connect import DatabaseError
 from flask import Flask, request, render_template
 from ml.parameters import *
 from ml.train import *
 from ml.test import *
 from ml.stock_prediction import create_model, load_data, model_name
 from tensorflow.keras.layers import LSTM
-import os, io
+import os
 import json
-from flask import Response
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
-from database import db, DatabaseError
-# from flask_sqlalchemy import SQLAlchemy
-import psycopg2
-from database.data_import import *
+from database import db
 import numpy
 from psycopg2.extensions import register_adapter, AsIs
 
+
 cursor = db().connect()
-
-
-
 
 app = Flask(__name__)
 
 def addapt_numpy_float32(numpy_float32):
     return AsIs(numpy_float32)
-
 register_adapter(numpy.float32, addapt_numpy_float32)
 
 def pred(ticker:str, years:int):
@@ -58,40 +53,29 @@ def process_input(request_data:str) -> np.array:
     assert len(parsed_body.shape) == 2, "'Input must be a 2-D array"
     return parsed_body
 
+def get_database_data(query, args=(), one=False):
+        cursor.execute(query, args)
+        r = [dict((cursor.description[i][0], value)\
+                    for i, value in enumerate(row)) for row in cursor.fetchall()]
+        cursor.close()
+        return (r[0] if r else None) if one else r
 
-def store_to_database():
-        con = Data()
-        try:
-            ticker = request.form.get('Stock Ticker Name')
-            years = request.form.get('Number of years', type=int)
-            prediction= pred(ticker,years)
-            con.add_data()#,{"ticker": ticker, "years": years, "Future_price":prediction})
-            return "Data successfully committed"
-        except DatabaseError:
-            raise DatabaseError("Unable to commit data")
-
-
-    
 @app.route("/")
 def home():
     return render_template("index.html")
 
 @app.route('/predict', methods = ["POST"])
 def predict_output():
-    # con = Data()
-    # try:
-    ticker = request.form.get('Stock Ticker Name')
-    years = request.form.get('Number of years', type=int)
-    prediction= pred(ticker,years)
-    # con = Data()
-    # con.add_data()
-    cursor.execute("INSERT INTO Data (ticker_name, years_analysed, Future_price)\
+    try:
+        ticker = request.form.get('Stock Ticker Name')
+        years = request.form.get('Number of years', type=int)
+        prediction= pred(ticker,years)
+        cursor.execute("INSERT INTO Data (ticker_name, years_analysed, Future_price)\
             VALUES (%s, %s, %s)" ,(ticker,years, prediction))
-    #cursor.commit()
-    print("Record added successfully")
-    return render_template("index.html", prediction_text="Future price after {} days is ${:.2f}".format(LOOKUP_STEP,prediction))
-    # except:
-    #     return error_check()
+        print("Record added successfully")
+        return render_template("index.html", prediction_text="Future price after {} days is ${:.2f}".format(LOOKUP_STEP,prediction))
+    except:
+        return error_check()
 
 
 @app.route('/results', methods = ["POST"])
@@ -102,11 +86,16 @@ def results_json():
         years = input_params[1]
         output = pred(ticker,years)
         return json.dumps({f"Future price after {LOOKUP_STEP} days is $": output.tolist()})
- 
 
-if __name__ == "__main__":
-    # app.run(debug=True)
+@app.route('/read_database', methods = ["GET"])
+def output():
+    my_query = get_database_data("SELECT * FROM Data LIMIT %s", (10,))
+    json_output = json.dumps(my_query, default=str)
+    return json_output
     
+
+ 
+if __name__ == "__main__":
     app.run(debug=True)
 
 
