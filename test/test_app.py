e7478fb212cd91a9500bd864a39602743eff3190
Abimbola Ojikutu <aojikutu20@aiip.alueducation.com>
2021-08-01 17:56:47 +0100

    updated test to not predict

--- test/test_app.py
+++ test/test_app.py
@@ -1,12 +1,5 @@
-import json
 from app import *
 
 def test_home(app, client):
     response = client.get("/")
     assert response.status_code == 200
-
-def test_predict(app, client):
-    response = app.test_client().post("/results",data=json.dumps({"inputs":[["FB",5]]}),
-    content_type="application/json") 
-    assert response.status_code == 200
-    assert response.data == b'{"Predicted House Price in Dollars": 28.99}'
