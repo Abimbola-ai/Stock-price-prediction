eb4cdb89572e4eba795cfe116993725b5d607fa7
Abimbola Ojikutu <aojikutu20@aiip.alueducation.com>
2021-08-01 18:19:06 +0100

    updated app to connect to the database

--- app.py
+++ app.py
@@ -34,26 +34,26 @@ def home():
 @app.route('/predict', methods = ["POST"])
 def predict_output()->str:
     """Takes in form data from user and returns future price and accuracy of prediction"""
-    # try:
-    ticker = request.form.get('Stock Ticker Name')
-    years = request.form.get('Number of years', type=int)
-    val = predict_future_price(ticker,years)
-    prediction = val[0]
-    lr_confidence = round(val[1] * 100,2)
-    price = np.round(prediction, decimals=2)
-    string_price = " ".join(map(str, price))
-    final_price = float(string_price)
-        # try:
-        #     cursor = db().connect()
-        #     cursor.execute("INSERT INTO Data (ticker_name, years_analysed, Future_price)\
-        #         VALUES (%s, %s, %s)" ,(ticker, years, final_price))
-        # except DatabaseError:
-        #     raise DatabaseError("Unable to add data")
+    try:
+        ticker = request.form.get('Stock Ticker Name')
+        years = request.form.get('Number of years', type=int)
+        val = predict_future_price(ticker,years)
+        prediction = val[0]
+        lr_confidence = round(val[1] * 100,2)
+        price = np.round(prediction, decimals=2)
+        string_price = " ".join(map(str, price))
+        final_price = float(string_price)
+        try:
+            cursor = db().connect()
+            cursor.execute("INSERT INTO Data (ticker_name, years_analysed, Future_price)\
+                VALUES (%s, %s, %s)" ,(ticker, years, final_price))
+        except DatabaseError:
+            raise DatabaseError("Unable to add data")
         
-    return render_template("index.html", prediction_text="{} price tomorrow will be ${:.2f} with a \
+        return render_template("index.html", prediction_text="{} price tomorrow will be ${:.2f} with a \
              confidence of {}%".format(ticker,final_price, lr_confidence))
-    # except:
-    #     return error_check()
+    except:
+        return error_check()
 
 
 @app.route('/results', methods = ["POST"])
