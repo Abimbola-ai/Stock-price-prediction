5cbb45d4b0a2f1680d04bb69159b74c9ba1475d8
Abimbola Ojikutu <aojikutu20@aiip.alueducation.com>
2021-08-01 17:29:39 +0100

    deleted testcode because it is not needed

--- testcode.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-url = 'http://127.0.0.1:5000/results'
-data = ["TSLA",3]
-j_data= json.dumps(data)
-headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-r = requests.post(url, data=j_data, headers=headers)
-print(r, r.text)
