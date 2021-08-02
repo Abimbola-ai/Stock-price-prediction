f37ddd8b08b6054ac1989f4a362d946b20cc2912
Abimbola Ojikutu <aojikutu20@aiip.alueducation.com>
2021-08-02 18:43:45 +0100

    updated app name

--- app.py
+++ app.py
@@ -17,14 +17,6 @@ def error_check()->str:
         return json.dumps({"error": "Prediction Failed"}), 500
 
 
-def get_database_data(query, args=(), one=False):
-    """Gets data from the postgres database hosted on heroku"""
-    cursor = db().connect()
-    cursor.execute(query, args)
-    r = [dict((cursor.description[i][0], value)\
-                    for i, value in enumerate(row)) for row in cursor.fetchall()]
-    cursor.close()
-    return (r[0] if r else None) if one else r
 
 @app.route("/")
 def home():
@@ -43,38 +35,12 @@ def predict_output()->str:
         price = np.round(prediction, decimals=2)
         string_price = " ".join(map(str, price))
         final_price = float(string_price)
-        try:
-            cursor = db().connect()
-            cursor.execute("INSERT INTO Data (ticker_name, years_analysed, Future_price)\
-                VALUES (%s, %s, %s)" ,(ticker, years, final_price))
-        except DatabaseError:
-            raise DatabaseError("Unable to add data")
         return render_template("index.html", prediction_text="{} price tomorrow will be ${:.2f} with a \
              confidence of {}%".format(ticker,final_price, lr_confidence))
     except:
         return error_check()
 
-@app.route('/results', methods = ["POST"])
-def results_json():
-    """Takes in form data from user and returns future price and accuracy of prediction in json format"""
-    data = request.get_json()
-    ticker = data[0]
-    years = data[1]
-    val = predict_future_price(ticker,years)
-    prediction = val[0]
-    lr_confidence = round(val[1] * 100,2)
-    price = np.round(prediction, decimals=2)
-    string_price = " ".join(map(str, price))
-    final_price = float(string_price)
-    return jsonify("Predicted price is tomorrow in dollars".format(final_price,lr_confidence))
 
-
-@app.route('/read_database', methods = ["GET"])
-def output():
-    """Prints out the first 10 rows in the database"""
-    my_query = get_database_data("SELECT * FROM Data LIMIT %s", (10,))
-    json_output = json.dumps(my_query, default=str)
-    return json_output
     
 
  
