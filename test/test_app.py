c7440942156c44e51fde2d953d0f398ab059fd44
Abimbola Ojikutu <aojikutu20@aiip.alueducation.com>
2021-08-03 12:30:18 +0100

    updated test file

--- test/test_app.py
+++ test/test_app.py
@@ -3,3 +3,8 @@ from app import *
 def test_home(app, client):
     response = client.get("/")
     assert response.status_code == 200
+
+def test_predict(app, client):
+    response = app.test_client().post("/results",data=json.dumps(["TSLA",5]),content_type="application/json") 
+    assert response.status_code == 200
+   
